d03fa7ce9645e5019820b2f3d2d2693dda056986
maxwell3025 <amoinus@gmail.com>
2024-02-17 18:23:30 -0500

    Added Correction to getFrames

--- getFrames.sh
+++ getFrames.sh
@@ -1,4 +1,5 @@
 wget https://ia802905.us.archive.org/19/items/TouhouBadApple/Touhou%20-%20Bad%20Apple.mp4
 mkdir splitVideo
 mv "Touhou - Bad Apple.mp4" splitVideo/bad_apple.mp4
-ffmpeg -i splitVideo/bad_apple.mp4 splitVideo/%04d.png
+ffmpeg -i splitVideo/bad_apple.mp4 -filter:v fps=12 splitVideo/bad_apple_fixed.mp4
+ffmpeg -i splitVideo/bad_apple_fixed.mp4 splitVideo/%04d.png
