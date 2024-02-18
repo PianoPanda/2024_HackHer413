0e0c58b5062aec814373b8904358d3631ff23c56
andOrlando <bennettgillig@gmail.com>
2024-02-17 21:25:29 -0500

    does stuff

--- index.js
+++ index.js
@@ -3,39 +3,56 @@ import * as fs from 'node:fs';
 import { png2PixelMatrix, scalePixelMatrix, printMatrix } from "./pixelManager.js";
 import { SeperateTo7Days } from "./scaleMatrix.js";
 import { authorize } from "./auth.js";
-import { writeFrame, writeBlock, wipeCalendar } from './calendar.js';
+import { writeBlock, log } from './calendar.js';
 import { buildCalendarColumn } from './eventManager.js';
 import { google } from 'googleapis';
+import process from "process"
 
 const HEIGHT = 70;
 const WIDTH = 112
 
 const auth = await authorize()
 const calendar = google.calendar({version: 'v3', auth});
-const CALENDAR_ID="43389c50be563ec14c5b7d28a65e3fc8081345a7c8c84cc8f225fa54f4139ee0@group.calendar.google.com"
 
-/*
-fs.readdir("./"+dir, (_, files) => {
+const CALENDAR_ID = (await calendar.calendarList.list()).data.items.filter(a => a.summary == "teest")[0].id
 
-  const a = files[84]
-  files = [a]; 
+fs.readdir("./"+dir, async (_, files) => {
+
+  let die = false
+  const redo = []
   
-  files.forEach((file, frame) => {
-    const matrixBeforResize = png2PixelMatrix(dir+'/'+file);
-    const matrixAfterResize = scalePixelMatrix(matrixBeforResize, WIDTH, HEIGHT);
+  let total = 0;
+  for (let i=0; i<files.length; i++) {
+    const matrixBeforResize = png2PixelMatrix(dir+'/'+files[i]);
+    const matrixAfterResize = scalePixelMatrix(matrixBeforResize, HEIGHT, WIDTH);
     const days = SeperateTo7Days(matrixAfterResize)
     const blocks = days.map(day => buildCalendarColumn(day))
 
-    // for (let i=0; i<blocks)
-    // writeBlock(0, blocks[0][0], async event => {
-      // console.log(event)
-      // calendar.events.insert({
-        // "calendarId": CALENDAR_ID,
-        // "resource": event
-      // }).then(console.log)
-    // })
-    
-    writeFrame(calendar, blocks, frame)
-  })
-})*/
-wipeCalendar(calendar)
+    for (let column=0; column<7; column++) { 
+      for (const block of blocks[column]) {
+        while (die) {
+          await new Promise(res => setTimeout(res, 10000))
+          process.stdout.write("waiting 10s")
+        }
+
+        writeBlock(calendar, CALENDAR_ID, i, column, block).then(()=>{}, ()=> {
+          redo.push([i, column, block])
+
+          console.log("died\n");
+          die = true
+          setTimeout(() => die = false, 60000)
+        })
+        log(column, i)
+
+        await new Promise((res)=>setTimeout(res, 300))
+      }
+    }
+    total += blocks.length
+  }
+  console.log(total)
+  console.log("redoing :)")
+  for (let i=0; i<redo.length; i++) {
+    console.log(`redoing ${redo[i]}`)
+    await writeBlock(calendar, CALENDAR_ID, ...redo[i])
+  }
+})
