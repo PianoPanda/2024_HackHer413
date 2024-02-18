6b6e42148a413f1a5a9b7165722bc60cb37d063e
andOrlando <bennettgillig@gmail.com>
2024-02-17 23:50:24 -0500

    do stuff kill me now

--- calendar.js
+++ calendar.js
@@ -1,12 +1,13 @@
 import { authorize } from "./auth.js"
 import { google } from 'googleapis';
 import process from "process"
+import { randomUUID } from "crypto";
 
 // const CALENDAR_ID="43389c50be563ec14c5b7d28a65e3fc8081345a7c8c84cc8f225fa54f4139ee0@group.calendar.google.com"
 const BASE_DATE = new Date("2024-02-11")
 
-export async function writeBlock(calendar, CALENDAR_ID, frame, column, { start, end, order, color }) {
-
+function makeEvent(frame, column, { start, end, order, color }) {
+  
   const t_start = new Date(BASE_DATE)
   t_start.setUTCDate(t_start.getUTCDate() + frame * 7 + column)
   t_start.setMinutes(t_start.getMinutes() + start * 15)
@@ -30,6 +31,12 @@ export async function writeBlock(calendar, CALENDAR_ID, frame, column, { start, 
     },
   }
   if (!color) event.colorId = "8"
+  return event
+}
+
+export async function writeBlock(calendar, CALENDAR_ID, frame, column, block) {
+  const event = makeEvent(frame, column, block)
+
   const res = calendar.events.insert({
     calendarId: CALENDAR_ID,
     resource: event
@@ -56,24 +63,31 @@ export async function writeFrame(calendar, blocks, frame) {
 
 }
 
-function writeFrameToICSs(blocks, frame) {
-  let white = ""
-  let black = ""
 
-  for (const block of blocks) {
-    writeBlock(frame, block, event => {
-      let stuff = "BEGIN:VEVENT\n"
-      const start = new Date(event.start.dateTime).getTime()
-      const end = new Date(event.end.dateTime).getTime()
-      stuff += `DTSTAMP:${start}\n`
-      stuff += `DTSTART:${start}\n`
-      stuff += `DTEND:${end}\n`
-      stuff += `SUMMART:${event.summary}\n`
-      stuff += "END:VEVENT\n"
-      if (event.colorId) black += stuff
-      else white += stuff
-    })
-  }
 
-  return [white, black]
+export function writeBlockToICAL(frame, column, block, cal, white) {
+  const event = makeEvent(frame, column, block)
+  const start = new Date(event.start.dateTime)
+  const end = new Date(event.end.dateTime)
+
+  if (!((event.colorId != undefined) == (white == 0))) return
+  
+  cal.createEvent({
+    start: start,
+    end: end,
+    summary: "a"
+  })
+  
+
+
+  // let stuff = "BEGIN:VEVENT\n"
+  // stuff += `UID:${randomUUID()}\n`
+  // stuff += `SUMMARY:${event.summary}\n`
+  // stuff += `DTSTAMP:${start}Z\n`
+  // stuff += `DTSTART:${start}Z\n`
+  // stuff += `DTEND:${end}Z\n`
+  // if (event.colorId) stuff += "COLOR:graphite\n"
+// c` stuff += "END:VEVENT\n"
+
+  // return stuff
 }
--- index.js
+++ index.js
@@ -3,10 +3,11 @@ import * as fs from 'node:fs';
 import { png2PixelMatrix, scalePixelMatrix, printMatrix } from "./pixelManager.js";
 import { SeperateTo7Days } from "./scaleMatrix.js";
 import { authorize } from "./auth.js";
-import { writeBlock, log } from './calendar.js';
+import { writeBlock, log, writeBlockToICAL,  } from './calendar.js';
 import { buildCalendarColumn } from './eventManager.js';
 import { google } from 'googleapis';
 import process from "process"
+import ical from 'ical-generator';
 
 const HEIGHT = 70;
 const WIDTH = 112
@@ -16,6 +17,7 @@ const calendar = google.calendar({version: 'v3', auth});
 
 const CALENDAR_ID = (await calendar.calendarList.list()).data.items.filter(a => a.summary == "teest")[0].id
 
+/*
 fs.readdir("./"+dir, async (_, files) => {
 
   let die = false
@@ -56,3 +58,64 @@ fs.readdir("./"+dir, async (_, files) => {
     await writeBlock(calendar, CALENDAR_ID, ...redo[i])
   }
 })
+*/
+
+
+const files = fs.readdirSync("./"+dir)
+function dostuff(name, fd, whiteblack) {
+  const cal = ical()
+
+  for (let i=files.length/300*fd|0; i<files.length/300*(fd+1)|0; i++) {
+    const matrixBeforResize = png2PixelMatrix(dir+'/'+files[i]);
+    const matrixAfterResize = scalePixelMatrix(matrixBeforResize, HEIGHT, WIDTH);
+    const days = SeperateTo7Days(matrixAfterResize)
+    const blocks = days.map(day => buildCalendarColumn(day))
+
+    for (let column=0; column<7; column++) { 
+      for (const block of blocks[column]) {
+        writeBlockToICAL(i, column, block, cal, whiteblack)
+        log(column, i)
+      }
+    }
+  }
+
+  fs.writeFileSync(name, cal.toString())
+}
+
+for (let n=0; n<1; n++) {
+  dostuff(`white${n}.ical`, 100, 0)
+}
+// for (let n=0; n<40; n++) {
+  // dostuff(`black${n}.ical`, n, 1)
+// }
+
+
+
+
+
+// dostuff("white1.ical", 0, 0)
+// dostuff("white2.ical", 1, 0)
+// dostuff("white3.ical", 2, 0)
+// dostuff("white4.ical", 3, 0)
+// dostuff("black1.ical", 0, 1)
+// dostuff("black2.ical", 1, 1)
+// dostuff("black3.ical", 2, 1)
+// dostuff("black4.ical", 3, 1)
+
+
+
+// const black = ical()
+
+// for (let i=0; i<files.length; i++) {
+  // const matrixBeforResize = png2PixelMatrix(dir+'/'+files[i]);
+  // const matrixAfterResize = scalePixelMatrix(matrixBeforResize, HEIGHT, WIDTH);
+  // const days = SeperateTo7Days(matrixAfterResize)
+  // const blocks = days.map(day => buildCalendarColumn(day))
+
+  // for (let column=0; column<7; column++) { 
+    // for (const block of blocks[column]) {
+      // writeBlockToICAL(i, column, block, black, 1)
+      // log(column, i)
+    // }
+  // }
+// }
