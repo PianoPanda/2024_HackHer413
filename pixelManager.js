08acf4bdb4bae27e478fa5eac23304191a072b30
pianoPanda <panda.piano.sgz@gmail.com>
2024-02-17 19:30:02 -0500

    Working Color Version
    Just run it and see

--- pixelManager.js
+++ pixelManager.js
@@ -73,13 +73,85 @@ export function png2PixelMatrix(framePath) {
     return binaryMatrix;
 }
 
-export 
+/**
+ * @type {([number, number, number])[]}
+ */
+const COLORS = [
+    [0xFF, 0xFF, 0xFF], //WHITE
+    [0xE6, 0x7C, 0x73], //FLAMINGO
+    [0xD5, 0x00, 0x00], //TOMATO
+    [0xF4, 0x51, 0x1E], //TANGERINE
+    [0xF6, 0xBF, 0x26], //BANANA
+    [0x33, 0xB6, 0x79], //SAGE
+    [0x03, 0x9B, 0xE5], //PEACOCK
+    [0x79, 0x86, 0xCB], //LAVENDER
+    [0x8E, 0x24, 0xAA], //GRAPE
+    [0x61, 0x61, 0x61], //GRAPHITE
+    [0x0B, 0x80, 0x43], //BASIL
+    [0x3F, 0x51, 0xB5] //BLUEBERRY
+];
+//console.log(COLORS.findIndex(color => color[0] == 0xD5 && color[1] == 0x00 && color[1] == 0x00))
+
+export function colorSimilarity(framePath) {
+    const image = loadImageFromFile(framePath);
+    const { width, height } = image;
+
+    const colorMatrix = [];
+    for (let y = 0; y < height; y++) {
+        const row = [];
+        for (let x = 0; x < width; x++) {
+            let closestColor = findClosestColor(image.getPixel(x, y)); //image.getPixel(x, y) returns a RGB value
+            // console.log(closestColor);
+            row.push(COLORS.findIndex(color => color[0]==closestColor[0] && color[1]==closestColor[1] && color[2]==closestColor[2]).toString(16));
+            // color => color[0] == 0xD5 && color[1] == 0x00 && color[1] == 0x00
+            //row.push(COLORS.findIndex(closestColor));
+        }
+        colorMatrix.push(row.join(''));
+    }
+
+    return colorMatrix;
+}
+
+function findClosestColor(rgbValue) {
+    let minDistance = Infinity;
+    let closestColor = null;
+
+    COLORS.forEach(color => {
+        const distance = calculateDistance(rgbValue, color);
+        if (distance < minDistance) {
+            minDistance = distance;
+            closestColor = color;
+        }
+    });
+
+    return closestColor;
+}
+
+/**
+ * 
+ * @param {[number, number, number]} rgb1 
+ * @param {[number, number, number]} rgb2 
+ * @returns {number}
+ */
+function calculateDistance(rgb1, rgb2) {
+    const sumOfSquares = rgb1.reduce((acc, val, index) => {
+        const diff = val - rgb2[index];
+        return acc + diff * diff;
+    }, 0);
+    return Math.sqrt(sumOfSquares);
+}
 
 // Example usage:
+//Black and White testing
 //const framePath = 'miku.png'; // Replace 'yingyang.png' with the path to your image file
-const framePath = 'yingyang.png';
-//const framePath = 'undertale.png';
-console.log(png2PixelMatrix(framePath).join('\n'));
+//const framePath = 'yingyang.png';
+// const framePath = 'undertale.png';
+//console.log(png2PixelMatrix(framePath).join('\n'));
+
+//Color testing
+//const framePath = 'stevenLookAtThis.png';
+const framePath = 'miku1.png';
+console.log(colorSimilarity(framePath).join('\n'));
 
 // width = # of col, height = # of row
 export function scalePixelMatrix(matrix, newWidth, newHeight) {
